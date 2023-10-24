2c5e45874ed727cd87fe623f93bbaf631fec8873
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-10-24 13:40:06 +0200

    Update - sw.js tilføjet

--- klimatossen/src/App.jsx
+++ klimatossen/src/App.jsx
@@ -8,17 +8,22 @@ import { Footer } from './component/footer/Footer'
 import { Header } from './component/header/Header'
 
 function App() {
-  
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+      .then(reg => console.log('service worker registered', reg))
+      .catch(err => console.error('service worker not registered', err))
+  }
 
   return (
     <>
-      <Header/>
-      <Quote/>
-      <Co2/>
-      <Klima/>
-      <Bidrage/>
-      <Arrangement/>
-      <Footer/>
+      <Header />
+      <Quote />
+      <Co2 />
+      <Klima />
+      <Bidrage />
+      <Arrangement />
+      <Footer />
     </>
   )
 }
---
+++ klimatossen/sw.js
@@ -0,0 +1,64 @@
+const staticCacheName = 'static-caches-v3'
+const dynamicCacheName = 'site-dynamic-v1'
+
+// Array med filer
+const assets = [
+    './index.html',
+    './src/main.jsx',
+    './src/App.jsx',
+    './src/App.css'
+]
+
+caches.open('my-cache').then(cache => {
+    // Tilføj flere filer
+    cache.addAll(assets); 
+});
+
+
+// Install Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+	console.log('Service Worker has been installed');
+})
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+	console.log('Service Worker has been activated');
+    // Bliver i scopet til task er udført
+    event.waitUntil(
+        //henter alle cache pakker
+        caches.keys().then(keys => {
+            //filtere array med alle uactuelle cache pakker
+            const filteredKeys = keys.filter(key => key !== staticCacheName)
+            //mapper array og sletter pakker
+            filteredKeys.map(key => {
+                caches.delete(key)
+            })   
+        })
+    )
+})
+
+// Fetch event
+self.addEventListener('fetch', event => {
+	// Kontroller svar på request
+	event.respondWith(
+		// Kig efter file match i cache 
+		caches.match(event.request).then(cacheRes => {
+			// Returner match fra cache - ellers hent fil på server
+			return cacheRes || fetch(event.request).then(fetchRes => {
+				// Tilføjer nye sider til cachen
+				return caches.open(dynamicCacheName).then(cache => {
+					// Bruger put til at tilføje sider til vores cache
+					// Læg mærke til metoden clone
+					cache.put(event.request.url, fetchRes.clone())
+					// Returnerer fetch request
+					return fetchRes
+				})
+			})
+		})
+	)
+})
